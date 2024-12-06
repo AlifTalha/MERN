38a964a87fb1324733611fb44d22928c7c36522e
ALIF HOSSAIN TALHA <131950495+AlifTalha@users.noreply.github.com>
2024-12-06 23:30:34 +0600

    Add files via upload

---
+++ BookStore-MERN-stack/frontend/src/App.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+// pages and components
+import Home from './pages/Home';
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Navbar />
+        <div className="pages">
+          {/* Use Routes instead of Routers */}
+          <Routes>
+            <Route 
+              path="/" 
+              element={<Home />} 
+            />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
