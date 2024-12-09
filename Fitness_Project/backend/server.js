212ba717b9cfd7f0c3c44a4d033f123db607f6d4
ALIF HOSSAIN TALHA <131950495+AlifTalha@users.noreply.github.com>
2024-12-09 14:44:18 +0600

    Add files via upload

---
+++ Fitness_Project/backend/server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+
+dotenv.config();
+connectDB();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Routes
+const exercisesRouter = require("./routes/exercises");
+const usersRouter = require("./routes/users");
+const authRouter = require("./routes/auth");
+
+app.use("/exercises", exercisesRouter);
+app.use("/users", usersRouter);
+app.use("/auth", authRouter);
+
+// Default route
+app.get("/", (req, res) => {
+  res.send("Hello to Fitness Tracker API");
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server is running on port: ${port}`);
+});
